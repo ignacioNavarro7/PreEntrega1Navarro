5b1789026e5734a3292c3789f0e1511c6e93dd79
ignacioNavarro7 <108034775+ignacioNavarro7@users.noreply.github.com>
2024-09-03 16:52:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/* function imprimirPrecios() {
+    const auto1 = "Suzuki";
+    const auto2 = "Fiat";
+    let precio1 = 12000;
+    let percio2 = 12500;
+}
+imprimirPrecios();
+
+NO LO SUPE HACER xd
+
+*/
+
+
+prompt("Cuál es el precio del auto?") 
+
+for (let i = 0; i < 5; i++) {
+    const auto = prompt("Ingresar marca de auto");
+    console.log("Su marca de auto es " + auto);
+}
+
+
